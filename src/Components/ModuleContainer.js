f00a048ee96a0f5923dde8ad1d5a0507f9781986
rnotaria <rnotaria776@gmail.com>
2021-08-30 00:12:36 -0500

    experimenting with layout

--- src/Components/ModuleContainer.js
+++ src/Components/ModuleContainer.js
@@ -2,20 +2,22 @@ import React, { useState } from "react";
 import SendModule from "./SendModule/SendModule";
 import ReceiveModule from "./ReceiveModule/ReceiveModule";
 import TabPanel from "./TabPanel";
-import styled from "styled-components";
-import styles from "../styles/styles.module.css";
 import Box from "@material-ui/core/Box";
 
-// const styles = {
-//   boxShadow: 1,
-//   // width: "500px",
-//   height: "100%",
-// };
+const moduleContainerStyle = {
+  minWidth: "400px",
+  width: "40vw",
+  height: "60vh",
+  display: "flex",
+  flexDirection: "column",
+};
 
-const Container = styled.div`
-  width: 500px;
-  background: beige;
-`;
+const moduleContentStyle = {
+  boxShadow: 1,
+  bgcolor: "background.paper",
+  padding: 1,
+  flexGrow: 1,
+};
 
 function ModuleContainer() {
   const [value, setValue] = useState(0);
@@ -27,10 +29,10 @@ function ModuleContainer() {
   const module = value === 0 ? <SendModule /> : <ReceiveModule />;
 
   return (
-    <div className={styles.moduleContainer}>
+    <Box {...moduleContainerStyle}>
       <TabPanel value={value} handleChangeValue={handleChangeValue} />
-      <Box>{module}</Box>
-    </div>
+      <Box {...moduleContentStyle}>{module}</Box>
+    </Box>
   );
 }
 
--- src/Components/SendModule/SendModule.js
+++ src/Components/SendModule/SendModule.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function SendModule() {
-  return <div></div>;
+  return <div>send</div>;
 }
 
 export default SendModule;
