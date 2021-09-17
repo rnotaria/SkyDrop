a84a58d119a807440a42635a2b0e7af374fdb705
rnotaria <rnotaria776@gmail.com>
2021-09-17 00:56:50 -0500

    refactored to zip file and send to AWS rather than each file

--- skydrop_server/app/handlers/sendHandler.go
+++ skydrop_server/app/handlers/sendHandler.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
+	"archive/zip"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/rnotaria/SkyDrop/app/awsServices"
 	"github.com/rnotaria/SkyDrop/app/tools"
 	"github.com/rnotaria/SkyDrop/utils"
+	"io/ioutil"
 	"mime/multipart"
 	"net/http"
 )
@@ -42,35 +45,27 @@ func (sendHandler *SendHandler) Send(w http.ResponseWriter, r *http.Request) {
 	address := tools.GenerateAddress(utils.NumberOfWords)
 	fmt.Println("Address:", *address)
 
-	// # # # # # # # # # Comment below to bypass AWS # # # # # # # # # # #
-	for i := range sendHandler.files {
-		filename := sendHandler.files[i].Filename
-		key := *address + "/" + filename
-
-		fmt.Println("Uploading", filename)
-
-		err := func() error {
-			file, err := sendHandler.files[i].Open()
-			if err != nil {
-				return err
-			}
-
-			defer file.Close()
-
-			_, err = sendHandler.S3Service.PutObject(&key, file)
-			if err != nil {
-				return err
-			}
-
-			return nil
-		}()
-
-		if err != nil {
-			fmt.Println(err)
-			http.Error(w, "Bad Request", http.StatusBadRequest)
-			return
-		}
+	err = makeZipFile(&sendHandler.files, *address)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+
+	// # # # # # # # # # Comment below to bypass AWS # # # # # # # # # # #
+	//key := *address
+	//zipFile, err := os.Open("data/" + *address + ".zip")
+	//defer zipFile.Close()
+	//
+	//if err != nil {
+	//	http.Error(w, err.Error(), http.StatusInternalServerError)
+	//	return
+	//}
+	//
+	//_, err = sendHandler.S3Service.PutObject(&key, zipFile)
+	//if err != nil {
+	//	http.Error(w, err.Error(), http.StatusInternalServerError)
+	//	return
+	//}
 	// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 	resp := ResponseData{
@@ -88,5 +83,44 @@ func (sendHandler *SendHandler) Send(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(respJson)
 
 	fmt.Println("All files successfully uploaded to", *address)
+
+	// TODO delete zip file
+
+
 	fmt.Println("Done")
+}
+
+func makeZipFile(fileList *[]*multipart.FileHeader, zipName string) error {
+	fmt.Println("Creating zip file")
+
+	buffer := new(bytes.Buffer)
+	zipWriter := zip.NewWriter(buffer)
+
+	for _, file := range *fileList {
+		zipFile, err := zipWriter.Create(file.Filename)
+		if err != nil {
+			return err
+		}
+
+		f, err := file.Open()
+		if err != nil {
+			return err
+		}
+
+		bodyBytes, err := ioutil.ReadAll(f)
+		_, err = zipFile.Write(bodyBytes)
+		if err != nil {
+			return err
+		}
+	}
+
+	err := zipWriter.Close()
+	if err != nil {
+		return err
+	}
+
+	ioutil.WriteFile("data/"+zipName+".zip", buffer.Bytes(), 0777)
+
+
+	return nil
 }
