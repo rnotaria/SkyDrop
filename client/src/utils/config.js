eaf5b6b9c74554e447b6a4463b444dfea3ad81c3
Rushi <40039068+rnotaria@users.noreply.github.com>
2021-10-04 18:39:29 -0500

    changed prod base url (#6)

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -3,7 +3,7 @@ const config = {
     baseUrl: "http://localhost:8080",
   },
   production: {
-    baseUrl: "https://gentle-oasis-02718.herokuapp.com/",
+    baseUrl: "https://skydrop123.herokuapp.com",
   },
 };
 
