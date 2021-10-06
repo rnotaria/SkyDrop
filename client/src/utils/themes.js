936e2a018febf24f18717878021075de9e431062
rnotaria <rnotaria776@gmail.com>
2021-10-06 18:25:00 -0500

    added dark theme shadows

--- client/src/utils/themes.js
+++ client/src/utils/themes.js
@@ -49,4 +49,7 @@ export const darkMode = createTheme({
       primary: "#1c4671",
     },
   },
+  shadows: {
+    4: "0px 2px 4px -1px rgba(100, 100, 100, 0),0px 4px 5px 0px rgba(100, 100, 100, 0.14),0px 1px 10px 0px rgba(100, 100, 100, 0.7)",
+  },
 });
